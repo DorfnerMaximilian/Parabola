d91aff258f03a89b0854ecf50484430bcf5f9810
Maximilian Dorfner <maximilian.dorfner@tum.de>
2024-02-09 15:28:41 +0100

    Build Substructure for modularized version of parabola

---
+++ parabola.py
@@ -0,0 +1,7 @@
+# Parabola/Parabola.py
+import sys
+from pathlib import Path
+
+# Add the path to the 'submodules' directory to the Python path
+submodules_path = Path(__file__).parent / 'submodules'
+sys.path.append(str(submodules_path))
