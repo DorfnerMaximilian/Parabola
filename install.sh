4889ab519147fd821fd7b5b6cccb231a2bc5ccb6
Maximilian Dorfner <maximilian.dorfner@tum.de>
2024-04-05 10:55:51 +0200

    Fixed Error in install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #Add Parabola to python path
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 file=~/.bashrc
-if ! grep -qF "$SCRIPT_DIR" <"$file" ; then echo PYTHONPATH='${PYTHONPATH}'":$SCRIPT_DIR" >> ~/.bashrc ; fi
+if ! grep -qF "$SCRIPT_DIR" <"$file" ; then echo export PYTHONPATH='${PYTHONPATH}'":$SCRIPT_DIR" >> ~/.bashrc ; fi
 #compile the CPP extension
 cd "./CPP_Extension/"
 make
